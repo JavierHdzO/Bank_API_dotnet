ff336b449e3380273537cabf41a728741452395e
JavierHdzO <fco_hernandez1705@hotmail.com>
2023-04-01 23:28:24 -0600

    040123 F [FEAT]: add LoginService to generate JWT

--- Models/Dtos/AuthDto.cs/LoginDto.cs
+++ Models/Dtos/AuthDto.cs/LoginDto.cs
@@ -9,5 +9,6 @@ public class LoginDto {
     public string Email {get; set;} = null!;
 
     [Required]
+    [MinLength(8)]
     public string Password {get; set;} = null!;
 }
--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
@@ -16,17 +16,29 @@ public class AuthService {
     private readonly BankContext _bankContext;
     private readonly ILogger<BankContext> _logger;
 
-    public AuthService(BankContext bankContext, ILogger<BankContext> logger, IConfiguration config){
+    private readonly IPasswordHasher<User> _passwordHasher;
+
+
+    public AuthService(BankContext bankContext, ILogger<BankContext> logger, IConfiguration config, IPasswordHasher<User> passwordHasher){
         _bankContext = bankContext;
         _logger = logger;
         _config = config;
+        _passwordHasher = passwordHasher;
     }
 
     public async Task<User?> GetUser(LoginDto loginDto){
 
         try
-        {
-            var user = await  _bankContext.Users.SingleOrDefaultAsync(  x => x.Email == loginDto.Email && x.Password == loginDto.Password);
+        {   
+            
+            var user = await  _bankContext.Users.SingleOrDefaultAsync(  x => x.Email == loginDto.Email );
+
+            if( user is null ) return null;
+            
+            PasswordVerificationResult verifyPassword = _passwordHasher.VerifyHashedPassword(user, user.Password, loginDto.Password);
+
+            if( verifyPassword != PasswordVerificationResult.Success ) return null;
+            
             return user;
         }
         catch (Exception exception)
