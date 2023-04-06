d30a72ad97fb12a4723b4b4c9409841b20aa57ec
JavierHdzO <fco_hernandez1705@hotmail.com>
2023-04-05 21:59:50 -0600

    040523 F [FEAT]: add  Client Dto Validations

--- Models/Dtos/Client/UpdateClientDto.cs
+++ Models/Dtos/Client/UpdateClientDto.cs
@@ -3,5 +3,22 @@ using System.ComponentModel.DataAnnotations;
 namespace bank_api.Models.Dtos;
 
 public class UpdateClientDto {
+    
+    [Required]
+    [MaxLength(50)]
+    [MinLength(2)]
+    public string Name {get; set;} = null!;
 
+    [Required]
+    [MaxLength(50)]
+    [MinLength(2)]
+    public string LastName {get; set;} = null!;
+
+    [Required]
+    [Range(18, 150)]
+    public short  Age {get; set;}
+
+    [Required]
+    [StringLength(1)]
+    public string Genre {get; set;} = null!;
 }
