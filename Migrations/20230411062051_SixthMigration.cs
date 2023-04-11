b7b66034e18c46fb5545d0718598d8090a7a6000
JavierHdzO <fco_hernandez1705@hotmail.com>
2023-04-11 00:27:07 -0600

    041123 F [CHORE]: add SixMigration

---
+++ Migrations/20230411062051_SixthMigration.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace bank_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SixthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Account_AccountType_AccountTypeId",
+                table: "Account");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Account_Clients_ClientId",
+                table: "Account");
+
+            migrationBuilder.DropTable(
+                name: "AccountType");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Account",
+                table: "Account");
+
+            migrationBuilder.RenameTable(
+                name: "Account",
+                newName: "Accounts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Account_ClientId",
+                table: "Accounts",
+                newName: "IX_Accounts_ClientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Account_AccountTypeId",
+                table: "Accounts",
+                newName: "IX_Accounts_AccountTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Accounts",
+                table: "Accounts",
+                column: "AccountId");
+
+            migrationBuilder.CreateTable(
+                name: "AccountTypes",
+                columns: table => new
+                {
+                    AccountTypeId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    RegDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTypes", x => x.AccountTypeId);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_AccountTypes_AccountTypeId",
+                table: "Accounts",
+                column: "AccountTypeId",
+                principalTable: "AccountTypes",
+                principalColumn: "AccountTypeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Clients_ClientId",
+                table: "Accounts",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_AccountTypes_AccountTypeId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Clients_ClientId",
+                table: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "AccountTypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Accounts",
+                table: "Accounts");
+
+            migrationBuilder.RenameTable(
+                name: "Accounts",
+                newName: "Account");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Accounts_ClientId",
+                table: "Account",
+                newName: "IX_Account_ClientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Accounts_AccountTypeId",
+                table: "Account",
+                newName: "IX_Account_AccountTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Account",
+                table: "Account",
+                column: "AccountId");
+
+            migrationBuilder.CreateTable(
+                name: "AccountType",
+                columns: table => new
+                {
+                    AccountTypeId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    RegDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountType", x => x.AccountTypeId);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Account_AccountType_AccountTypeId",
+                table: "Account",
+                column: "AccountTypeId",
+                principalTable: "AccountType",
+                principalColumn: "AccountTypeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Account_Clients_ClientId",
+                table: "Account",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
